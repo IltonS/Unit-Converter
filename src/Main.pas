e6f7ab188efd51de0032ae424b4886f5ae85c316
IltonS <ilton.sequeira.s@gmail.com>
2024-03-08 23:31:28 -0300

    Add CarregarListas methods

--- src/Main.pas
+++ src/Main.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Styles.Objects, FMX.Edit, FMX.ListBox;
+  FMX.Controls.Presentation, FMX.Styles.Objects, FMX.Edit, FMX.ListBox,
+  FMX.Memo.Types, FMX.ScrollBox, FMX.Memo, System.JSON, FMX.Platform.Android;
 
 type
   TFrmMain = class(TForm)
@@ -20,6 +21,7 @@ type
     LblFormula: TLabel;
     LblUnidadeOrigem: TLabel;
     LblUnidadeDestino: TLabel;
+    MmConversor: TMemo;
     procedure FormCreate(Sender: TObject);
     procedure CmbMedidasChange(Sender: TObject);
     procedure CmbUnidadeOrigemChange(Sender: TObject);
@@ -29,6 +31,9 @@ type
   private
     { Private declarations }
     ValorBase: double;
+    Conversor: TJSONObject;
+    Medidas: TJSONArray;
+    Formulas: TJSONArray;
 
     procedure CarregarListaMedidas;
     procedure CarregarListaUnidades;
@@ -44,6 +49,9 @@ var
   FrmMain: TFrmMain;
 
 implementation
+
+uses
+  Androidapi.Helpers;
 
 {$R *.fmx}
 
@@ -70,13 +78,45 @@ begin
 end;
 
 procedure TFrmMain.CarregarListaMedidas;
+var
+  I: Integer;
+  Medida: TJSONObject;
 begin
-  //
+  for I:=0 to Medidas.Count-1 do
+  begin
+    Medida := Medidas.Items[I] as TJSONObject;
+    CmbMedidas.Items.Add(Medida.GetValue('Medida').Value);
+  end;
 end;
 
 procedure TFrmMain.CarregarListaUnidades;
+var
+  I,J: Integer;
+  MedidaSelecionada: string;
+  Medida, Formula: TJSONObject;
 begin
-  //
+  MedidaSelecionada := CmbMedidas.Items[CmbMedidas.ItemIndex];
+
+  CmbUnidadeOrigem.Clear;
+  CmbUnidadeDestino.Clear;
+
+  for I:=0 to Medidas.Count-1 do
+  begin
+    Medida := Medidas.Items[I] as TJSONObject;
+
+    if Medida.GetValue('Medida').Value = MedidaSelecionada then
+    begin
+      Formulas := Medida.GetValue('Formulas') as TJSONArray;
+
+      for J:=0 to Formulas.Count-1 do
+      begin
+        Formula := Formulas.Items[J] as TJSONObject;
+
+        CmbUnidadeOrigem.Items.Add(Formula.GetValue('Nome').Value);
+        CmbUnidadeDestino.Items.Add(Formula.GetValue('Nome').Value);
+      end;
+    end;
+  end;
 end;
 
 procedure TFrmMain.CmbMedidasChange(Sender: TObject);
@@ -138,7 +178,14 @@ begin
 end;
 
 procedure TFrmMain.FormCreate(Sender: TObject);
+const
+  WordBlue = TAlphaColor($FF2B579A);
 begin
+  TAndroidHelper.Activity.getWindow.setStatusBarColor(TAlphaColorRec.ColorToRGB(WordBlue));
+
+  Conversor := TJSONObject.ParseJSONValue(MmConversor.Lines.Text) as TJSONObject;
+  Medidas := Conversor.GetValue('Medidas') as TJSONArray;
+
   CmbMedidas.Tag := 1;
   CarregarListaMedidas;
   CmbMedidas.ItemIndex := 0;
